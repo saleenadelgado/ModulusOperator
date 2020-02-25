3fb803ba118040cc9b0c7d63f513d8b38f8170fa
saleenadelgado <Saleena.delgado.2022@student.lhschools.org>
2020-02-25 12:10:37 -0500

    OddnessMethod created

---
+++ src/com/ModulusOperator/Main.java
@@ -0,0 +1,31 @@
+package com.ModulusOperator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        testOddness();
+
+    }
+
+    public static void testOddness() {
+        // todo even or odd
+        // todo keep number contained
+        // todo get the last digit
+
+        // We want to find if a number is even or odd
+        int number;
+
+
+
+        Scanner keyboard;
+        keyboard= new Scanner(System.in);
+
+        System.out.println("Give me a positive number");
+        number = keyboard.nextInt();
+        System.out.println("Number is even: " + (number % 2 == 0));
+
+        System.out.println("Number is odd: " + (number % 2 != 0));
+    }
+}
