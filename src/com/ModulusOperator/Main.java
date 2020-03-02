a7209f3e7710e5502ff6c9351590a966b3481ac2
saleenadelgado <Saleena.delgado.2022@student.lhschools.org>
2020-03-02 12:17:35 -0500

    keepContainedMethodCreated

--- src/com/ModulusOperator/Main.java
+++ src/com/ModulusOperator/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         testOddness();
-
+        keepContained();
     }
 
     public static void testOddness() {
@@ -27,5 +27,22 @@ public class Main {
         System.out.println("Number is even: " + (number % 2 == 0));
 
         System.out.println("Number is odd: " + (number % 2 != 0));
+    }
+
+    public static void keepContained() {
+        int number;
+        Scanner keyboard;
+        keyboard= new Scanner(System.in);
+
+        System.out.println("Give me a positive number");
+        number = keyboard.nextInt();
+
+        System.out.println((number%8)+5);
+
+
+
+
+
+
     }
 }
